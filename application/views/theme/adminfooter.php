6f0459f3e1d617205a80ce6fab9104b0868a381d
T14\wowos <wowosegainaru@gmail.com>
2024-10-03 10:37:34 +0700

    minu

---
+++ application/views/theme/adminfooter.php
@@ -0,0 +1,5 @@
+<div class="container my-auto">
+          <div class="copyright text-center my-auto">
+            <span>Copyright &copy; 2020 Muris Studio Administrator System | Distributed by <a href="https://blogbugabagi.blogspot.com" target="_blank" rel="noopener noreferrer">BlogBugaBagi</a></span>
+          </div>
+        </div>
